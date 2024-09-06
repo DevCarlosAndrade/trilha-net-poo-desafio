6abcdad9f865ec3336176c36768edd222c00658a
DevCarlosAndrade <carlos.ed.andrade97@gmail.com>
2024-09-06 00:30:45 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("12345", "Nokia XYZ", "111111111", 64);
+
+Smartphone iphone = new Iphone("67890", "iPhone 12 pro MAX", "222222222", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.ReadLine();
